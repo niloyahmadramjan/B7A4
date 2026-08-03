ab07ebfa359fb2854d44610725d23a888aa6eaa4
RAMJAN <niloyahmadramjan@gmail.com>
2026-08-03 14:56:23 +0800

    updated

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -79,7 +79,7 @@ const getCurrentUser = catchAsync(async (req:Request, res:Response, next: NextFu
 const getDashboardInfo = catchAsync(async (req:Request, res:Response, next: NextFunction) => {
     const userId = req.user?.id
    
-    const result = await authService.getCurrentLoginUser(userId as string)
+    const result = await authService.dashboardInfo(userId as string)
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
